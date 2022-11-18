0ad842088f38745db03b3a556fd476f79c2c094a
keiji.ue <keiji.ue@gmail.com>
2022-11-18 09:30:09 +0900

    add kind playground

--- kind-playground/background.sh
+++ kind-playground/background.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
-echo "Setup kind-command" >> /var/log/background.log
+LOG='/var/log/background.log'
+KIND_VERSION='v0.17.0'
 
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64
+
+# Setup Kind Command
+echo "Setup kind-command" >> ${LOG}
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/${KIND_VERSION}/kind-linux-amd64
 chmod +x ./kind
 mv ./kind /usr/local/bin/kind
+
+# Setup Kind Config
+echo "Setup kind.yaml" >> ${LOG}
+cat <<EOF > kind.yaml
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+- role: worker
+- role: worker
+EOF
+
+# Create kind cluster
+echo "Create Kind Cluster" >> ${LOG}
+kind create cluster --config kind.yaml
 
 echo 'done' > /tmp/background-finished
 echo "Done"
--- kind-playground/foreground.sh
+++ kind-playground/foreground.sh
@@ -1,3 +1,3 @@
 echo waiting for init-background-script to finish
-while [ ! -f /tmp/background-finishd ]; do sleep 1; done
+while [ ! -f /tmp/background-finished ]; do sleep 1; done
 echo Hello and Welcom to this scenario!
