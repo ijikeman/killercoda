871b0484ae1b8f3fa4145534a2773e3715f0e636
keiji.ue <keiji.ue@gmail.com>
2022-12-07 15:23:52 +0900

    background.sh

--- kubernetes-kubeadm-1node/foreground.sh
+++ kubernetes-kubeadm-1node/foreground.sh
@@ -1,5 +1,4 @@
 while [ ! -f /tmp/background-finished ]; do sleep 5; echo 'check file'; done
 
-kind create cluster --config /tmp/kind.yaml
 source ~/.bashrc
 echo Hello and Welcom to this scenario!
