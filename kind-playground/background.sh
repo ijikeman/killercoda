41c3ef593d2b69908d7ed0b006c58a45fdc1f785
keiji.ue <keiji.ue@gmail.com>
2022-12-07 09:31:11 +0900

    background.sh

--- kind-playground/background.sh
+++ kind-playground/background.sh
@@ -41,3 +41,24 @@ EOF
 
 echo 'done' > /tmp/background-finished
 echo "Done"
+
+# Setup krew
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+e
+echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> ~/.bashrc
+
+# Setup Stern
+kubectl krew install stern
+
+# Setup kube-ps1
+PS1_VERSION=v0.8.0
+curl https://raw.githubusercontent.com/jonmosco/kube-ps1/${PS1_VERSION}/kube-ps1.sh -o /usr/local/bin/kube-ps1.sh
+echo 'source /usr/local/bin/kube-ps1.sh' >> ~/.bashrc
+echo "PS1='[\u@\h \W $(kube_ps1)]\$ '" >>  ~/.bashrc
